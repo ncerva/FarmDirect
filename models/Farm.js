7edec71aeeeb11b1442a72e6d898490b5ab08b60
Reid Johnson <rjsa210@gmail.com>
2021-06-22 19:36:00 -0700

    testing for heroku

---
+++ models/Farm.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FarmSchema = new Schema({
+  
+
+  
+
+});
+
+const Farm = mongoose.model('Farm', FarmSchema);
+
+module.exports = Farm;
