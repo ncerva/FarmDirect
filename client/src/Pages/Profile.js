ba409e7a84286a8ca800b05d262d88e89599cdd9
Nicole Tribo <Nicoletribo@yahoo.com>
2021-06-28 21:47:47 -0500

    got the basic style and functionality of the profile cards and results card

---
+++ client/src/Pages/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfileCard from "../components/ProfileCard";
+
+const Profile = () => {
+    return (
+        <ProfileCard />
+    );
+};
+
+export default Profile;
--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,46 +1,47 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
 
 
 
 export function Carousel() {
     return (
-        <Carousel fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+            <div className="carousel-item">
+                <img src="https://user-images.githubusercontent.com/76696641/123293878-f8143e00-d4d9-11eb-99eb-6af768476efe.png" className="d-block w-100" alt="First Slide" />
+                <div className="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div className="carousel-item">
+                <img src="https://user-images.githubusercontent.com/76696641/123294645-a7e9ab80-d4da-11eb-855e-ef381e38f98b.png" className="d-block w-100" alt="Second Slide" />
+                <div className="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div className="carousel-item active">
+                <img src="https://user-images.githubusercontent.com/76696641/123294912-e1bab200-d4da-11eb-9c32-e10c84a8e192.png" className="d-block w-100" alt="Third Slide" />
+                <div className="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+        </button>
+    </div>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
     )
 }
