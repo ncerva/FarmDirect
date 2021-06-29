c2885dbe7e035a7adb2fa72f5132df8f6017a33e
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-29 12:28:14 -0500

    background set

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Carousel } from "../components/Carousel";
-import { CarouselData } from "../components/Carousel/CarouselData";
+import { Carousel } from "../components/Carousel/index";
+import { CarouselData } from "../components/Carousel/CarouselData"
 
 
 const Home = () => {
     return (
     <div>
-        <Carousel slides={CarouselData} />
+        <div className="background-image: url('https://user-images.githubusercontent.com/76188076/123734826-a960fe00-d863-11eb-8f83-abef5dc64d25.jpeg');">
+                <img className="background" src="https://user-images.githubusercontent.com/76188076/123734826-a960fe00-d863-11eb-8f83-abef5dc64d25.jpeg" alt="" ></img>
+        <Carousel slides={CarouselData}/>
+        </div>
     </div>
     );
 };
