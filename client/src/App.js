1bba234a24312a78041cab9b9a0a97782a50b2d3
Reid Johnson <rjsa210@gmail.com>
2021-06-30 22:20:14 -0700

    routing to farm portal working -RJ

--- client/src/App.js
+++ client/src/App.js
@@ -23,29 +23,29 @@ function App() {
   return (
     <Router>
       <AuthContext.Provider value={{
-        ...authState, 
+        ...authState,
         setAuthState: (token, isAuthorized, isFarmer, currentUser) => setAuthState({
           token: token,
           isAuthorized: isAuthorized,
           isFarmer: isFarmer,
           currentUser: currentUser
         })
-        }}>
-      <div>
-        <Navbar />
-        <Switch>
-          
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/results" component={Results} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/farmerportal" component={FarmerPortal} />
-          
-        </Switch>
-        <Footer />
-      </div>
+      }}>
+        <div>
+          <Navbar />
+          <Switch>
+
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/results" component={Results} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/farmerportal" component={FarmerPortal} />
+
+          </Switch>
+          <Footer />
+        </div>
       </AuthContext.Provider>
     </Router>
   );
