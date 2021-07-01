1b2fedad3ff74d2049e1b1b7c2bfa83c0fb232a3
Nicole Tribo <Nicoletribo@yahoo.com>
2021-06-30 21:07:41 -0500

    started seed

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,3 +5,7 @@ mongoose.connect(
     process.env.MONGODB_URI ||
     "mongodb://localhost/farm-direct"
 );
+
+const farmerData = require("./farmerData.json");
+const farmeData = require("./farmData.json");
+
