f2b0746dcab08da213d55cda7805478213fa3c18
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-07-02 10:01:47 -0500

    removed background from homepage

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -6,11 +6,8 @@ import { CarouselData } from "../components/Carousel/CarouselData"
 const Home = () => {
     return (
     <div>
-        <div className="background-image: url('https://user-images.githubusercontent.com/76188076/123734826-a960fe00-d863-11eb-8f83-abef5dc64d25.jpeg');">
-                <img className="background" src="https://user-images.githubusercontent.com/76188076/123734826-a960fe00-d863-11eb-8f83-abef5dc64d25.jpeg" alt="" ></img>
         <Carousel slides={CarouselData}/>
         </div>
-    </div>
     );
 };
 
