05a23c6296ffdb65be7326c7d6e82d8923f0a224
Nicole Tribo <Nicoletribo@yahoo.com>
2021-07-02 20:15:42 -0500

    working on seed data

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -7,5 +7,40 @@ mongoose.connect(
 );
 
 const farmerData = require("./farmerData.json");
-const farmeData = require("./farmData.json");
+const farmData = require("./farmData.json");
+const productData = require("./productData.json");
 
+db.Farmer
+    .remove({})
+    .then(() => db.Farmer.collection.insertMany(farmerData))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+
+db.Farm
+    .remove({})
+    .then(() => db.Farm.collection.insertMany(farmData))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+db.Product
+    .remove({})
+    .then(() => db.Product.collection.insertMany(productData))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
