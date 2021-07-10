4ca3970952c11f27455cf2be3a95e9a2dd665f4e
Nicole Tribo <Nicoletribo@yahoo.com>
2021-07-10 10:57:40 -0500

    fixed the images on the results card and the profile card

--- client/src/components/ResultsCard/index.js
+++ client/src/components/ResultsCard/index.js
@@ -14,30 +14,23 @@ function ResultsCard(props) {
   return (
     <div>
       <div className="card">
-        <header className="card-header">
-          <p className="title">
-            props.heading
-          </p>
-        </header>
+        {/* <header className="card-header">
+        </header> */}
         <div className="card-content">
           <div className="content">
             <div className="media">
               <figure className="media-left">
-                <a href="">
                   <Link
                   to={linkFarmerProfile}>
-                  <p className="image is-128x128" note="thumbnail">
-                    <img src={props.image} alt="Placeholder image" />
-                  </p>
+                  {/* <p className="is-128x128" note="thumbnail"> */}
+                    <img  src={props.image} width="128" alt="Placeholder image" />
+                  {/* </p> */}
                   </Link>
-                </a>
               </figure>
               <div className="media-content">
                 <div className="content">
                   <p>
                     <strong className="title">{props.farm_name}</strong>
-                    <br />
-                    <strong className="subtitle">props.farmer</strong>
                     <br />
                     {props.description}
                     <br />
