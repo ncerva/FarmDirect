0c871bdc670b9eb001e626524ac103fd95a49471
Reid Johnson <rjsa210@gmail.com>
2021-07-11 23:23:52 -0700

    Cart context working, products adding,  checkout page working -RJ

--- client/src/components/ProfileProductBlock/index.js
+++ client/src/components/ProfileProductBlock/index.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./style.css"
 import API from "../../utils/API"
 import AuthContext from "../../utils/AuthContext";
+import { CartContext, CartProvider } from "../../utils/CartContext";
 import { MdAddShoppingCart } from "react-icons/md";
 import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+import { set } from "mongoose";
 
 function ProfileProductBlock(props) {
 
   const [count, setCount] = useState(1);
+  const [itemList, setItemList] = useState([])
 
-  const handleAddToCart = () => {
+  const total = props.price * count;
+
+  const addAlert = () => {
     alert(`${count} ${props.packsize} ${props.title} added to cart!`)
     setCount(1);
   }
@@ -24,14 +29,26 @@ function ProfileProductBlock(props) {
   }
 
 
-  return (
-    <div className="box has-background-primary-light is-flex is-justify-content-space-evenly">
-      <span>{props.title}</span>
-      <span>${props.price}/{props.packsize}</span>
-      <span><a onClick={countMinus}><FiMinusCircle /></a><span>{count}</span><a onClick={countPlus}><FiPlusCircle /></a></span>
-      <button className="button is-danger" onClick={handleAddToCart}><MdAddShoppingCart /></button>
-    </div>
-  )
+ 
+    const [cart, setCart] = useContext(CartContext)
+
+
+      const addToCart = () => {
+        const item = { title: props.title, price: props.price, packsize: props.packsize, total: total, quantity: count, id: props.id };
+        setCart(current => [...current, item]);
+        addAlert();
+        console.log(cart);
+      }
+      return (
+        <div key={props._id} className="box has-background-primary-light is-flex is-justify-content-space-evenly">
+          <span>{props.title}</span>
+          <span>${props.price}/{props.packsize}</span>
+          <span><a onClick={countMinus}><FiMinusCircle /></a><span>{count}</span><a onClick={countPlus}><FiPlusCircle /></a></span>
+          <button className="button is-danger" onClick={addToCart}><MdAddShoppingCart /></button>
+        </div>
+      )
+    
+  
 }
 
 export default ProfileProductBlock;
